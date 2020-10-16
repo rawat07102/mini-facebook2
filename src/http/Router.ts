1d3dcc5fbae856b32c2107e52527bf1b3ccb72ba
rawat07102 <ritikrawat0710@gmail.com>
2020-10-16 20:51:02 +0530

    update router class

---
+++ src/http/Router.ts
@@ -0,0 +1,51 @@
+import {NextApiHandler, NextApiRequest, NextApiResponse} from "next"
+import {
+	HTTPMethod,
+	MethodNotAllowedException,
+} from "./exception/MethodNotAllowedException"
+
+export class Router {
+	constructor() {}
+
+	get(handler: NextApiHandler) {
+		return async function routeHandler(
+			req: NextApiRequest,
+			res: NextApiResponse
+		) {
+			try {
+				if (req.method !== HTTPMethod.GET) {
+					throw new MethodNotAllowedException(
+						"Wrong Method",
+						req.url!,
+						HTTPMethod.GET
+					)
+				}
+				await handler(req, res)
+			} catch (err) {
+				return res.json({ok: false, data: null, error: err})
+			}
+		}
+	}
+
+	post(handler: NextApiHandler) {
+		return async function routeHandler(
+			req: NextApiRequest,
+			res: NextApiResponse
+		) {
+			try {
+				if (req.method !== HTTPMethod.POST) {
+					throw new MethodNotAllowedException(
+						"Wrong Method",
+						req.url!,
+						HTTPMethod.POST
+					)
+				}
+				await handler(req, res)
+			} catch (err) {
+				return res.json({ok: false, data: null, error: err})
+			}
+		}
+	}
+}
+
+export const router = new Router()
